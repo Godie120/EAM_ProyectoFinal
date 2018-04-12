f581c8881ef93bbd69525fcf291e6198076554e8
Godie <godie120@hotmail.com>
2018-04-12 17:12:10 -0500

    Modificacion

--- src/app/components/todo/todo.component.ts
+++ src/app/components/todo/todo.component.ts
@@ -35,9 +35,7 @@ export class TodoComponent {
 	getTodoList() {
 		this.todoService.getTodoList().subscribe((content) => {
 			this.todoListAll$ = content;
-			// Se valida si se está empleando un filtro actualmente.
 			this.filterTodo ? this.getTodoFilter(this.statusTodo) : this.getTodoAll();
-			// Se realiza el conteo de los ToDo
 			this.countAllTodo();
 		});
 	}
@@ -54,10 +52,8 @@ export class TodoComponent {
    * Función que carga todos los ToDo.
    */
 	countAllTodo() {
-		// Se reinician los contadores.
 		this.cantTodoComplete = 0;
 		this.cantTodoIncomplete = 0;
-		// Se recorre el listado completo de los ToDo.
 		this.todoListAll$.map((todo) => {
 			todo.status ? this.cantTodoComplete++ : this.cantTodoIncomplete++;
 		});
@@ -73,7 +69,6 @@ export class TodoComponent {
 	getTodoFilter(status: boolean) {
 		this.filterTodo = true;
 		this.statusTodo = status;
-		// Se asigna al listado que será mostrado los ToDo que cumplan con el filtro.
 		this.listTodo = this.todoListAll$.filter((todo) => todo.status === this.statusTodo);
 	}
 
@@ -81,9 +76,7 @@ export class TodoComponent {
    * Función empleado para agregar un elemento más a la lista de ToDo.
    */
 	addTodo(input: HTMLInputElement) {
-		// Se valida que el input no esté vacío.
 		if (input.value !== '') {
-			// Se crea el ToDo y se envía al servicio de registro.
 			this.todoService.addTodo(input.value);
 			input.value = null;
 		}
@@ -117,7 +110,6 @@ export class TodoComponent {
    * Función que permite eliminar los ToDo que se encuentran completos.
    */
 	clearTodoCompleted() {
-		// Se aplica un filtro para seleccionar los ToDo que están completos.
 		const clearTodo: ToDo[] = this.todoListAll$.filter((todo) => todo.status === true);
 		this.todoService.clearTodoCompleted(clearTodo);
 	}
@@ -126,15 +118,12 @@ export class TodoComponent {
    * Función que permite cambiar el estado de todos los ToDo.
    */
 	updateStatusTodo() {
-		// Se valida si falta algún ToDo por completar.
 		this.cantTodoIncomplete > 0
-			? // Si todos los ToDo están completos se les cambia el estado.
-				this.todoService.updateStatusTodo(
+			? this.todoService.updateStatusTodo(
 					this.todoListAll$.filter((todo) => todo.status === false),
 					true
 				)
-			: // Si algún ToDo está incompleto se completa.
-				this.todoService.updateStatusTodo(
+			: this.todoService.updateStatusTodo(
 					this.todoListAll$.filter((todo) => todo.status === true),
 					false
 				);
